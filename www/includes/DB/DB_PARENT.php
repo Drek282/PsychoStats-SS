6bdeed052ef07c078b3472e69bb739b558f7b2b8
Owen Lee Stairs <llewen@OWEN.cc-lan>
2024-01-06 18:29:01 -0400

    fix to logic in column_exists function

--- www/includes/DB/DB_PARENT.php
+++ www/includes/DB/DB_PARENT.php
@@ -310,7 +310,7 @@ function table_exists($tbl) {
 
 // returns true if a column exists in a table or tables based on the key=id given
 function column_exists($tbls, $cols) {
-	$ca = explode(', ', $cols);
+	$ca = (str_contains($cols, ', ')) ? explode(', ', $cols) : $cols;
 	if (is_array($ca)) {
 		$f = 0;
 		foreach ($ca as $c) {

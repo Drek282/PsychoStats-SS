48a11a3c6447824d483b45c84f2698c6a89c1e07
Owen Lee Stairs <llewen@OWEN.cc-lan>
2024-01-22 11:30:09 -0400

    * [Changed]  Minor tweaks.

--- www/install/migrations/000_base_20240121_00_credits.sql
+++ www/install/migrations/000_base_20240121_00_credits.sql
@@ -7,11 +7,11 @@ INSERT INTO `psss_config` (`id`, `conftype`, `section`, `var`, `value`, `label`,
                 <li><strong>Jason Morriss, a.k.a. Stormtrooper</strong>—the original creator of PsychoStats</li>
                 <li><a href="http://www.scoresheet.com/"><strong>Scoresheet Fantasy Sports</strong></a>—for the most addictive and compelling fantasy baseball experience available anywhere</li>
                 <li><strong>wakachamo, Rosenstein, Solomenka and janzagata</strong>—for contributions to the code</li>
-                <li><a href="https://www.behance.net/alessandroart"><strong>Alessandro Poli</strong></a>—for the most excellent rat used in the VRAT logo</li>
-                <li><a href="https://openclipart.org/artist/GusEinstein"><strong>Gustavo Ferreira</strong></a>—for the bat used in the VRAT logo</li>
+                <li><a href="https://www.behance.net/alessandroart"><strong>Alessandro Poli</strong></a>—for the most excellent rat used in the VRat logo</li>
+                <li><a href="https://openclipart.org/artist/GusEinstein"><strong>Gustavo Ferreira</strong></a>—for the bat used in the VRat logo</li>
                 <li><strong>RoboCop from APG and Mike Gasson</strong>—for feedback, support and encouragement</li>
                 <li>PsychoStats makes use of various open source libraries, some precompiled.  Among these libraries are jQuery, the Smarty Template Engine and JpGraph.  Most of the versions used in PsychoStats are obsolete but still functional and secure.  PsychoStats would not function without them and a special debt of gratitude is owed to the creators and maintainers of those libraries.</li>
             </ul>
             </div>',
             'Credits','textarea',0,'','','This is the content of the Credits for PsychoStats for Scoresheet Baseball.  You can edit this to create your own custom thank you list.  It uses html formatting.'),
-            (5015,'theme','credits',NULL,'Credits for PsychoStats for Scoresheet Baseball.','Credits','none',1,NULL,NULL,NULL);
+        (5015,'theme','credits',NULL,'Credits for PsychoStats for Scoresheet Baseball.','Credits','none',1,NULL,NULL,NULL);

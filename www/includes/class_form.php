252da5e249b2935dece3395ba774fd27bfbf4269
Owen Lee Stairs <llewen@OWEN.cc-lan>
2024-02-23 14:52:13 -0400

    * [Fixed] Broken ACP search functionality.

--- www/includes/class_form.php
+++ www/includes/class_form.php
@@ -205,7 +205,7 @@ function values() {
 }
 
 function value($var, $modified = false) {
-	return $modified ? $this->modify($var) : $this->input[$var];
+	return $modified ? $this->modify($var) : $this->input[$var] ?? null;
 }
 
 // get/set an error string for a field

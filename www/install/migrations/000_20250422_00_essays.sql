37aa725d29ee1e42c0e7c5696a3bd9f7118a3101
Owen Lee Stairs <llewen@OWEN.cc-lan>
2025-04-22 13:13:55 -0300

    * [Added] Help entries.

---
+++ www/install/migrations/000_20250422_00_essays.sql
@@ -0,0 +1,86 @@
+INSERT INTO `psss_config_help` (`id`, `enabled`, `idx`, `title`, `content`, `img`, `weight`) VALUES
+(100, 1, 1000, 'We All Need Noobs', 
+
+'<p>I\'ve been active in a number of online communities over the past few decades.  It\'s pretty easy to separate the healthy communities from the unhealthy ones.  It all begins with the way new members are treated.</p>
+
+<p>Healthy online communities understand that online there is always a great deal of \"churn\", and their survival depends on a constant supply of new members.  Healthy online communities treat new members with respect and kindness, and they put up with them wanting to discuss the same old topics, over and over again, because for the new member, those topics aren\'t old, they are new and fresh.</p>
+
+<p>I played Scoresheet way back in 1996 and then took a two decade sabbatical from baseball.  In 2019 and 2020 I picked up a couple of Scoresheet teams.  So even though I\'ve played a lot of fantasy baseball in my time, I was essentially a noob.</p>
+
+<p>In 2021 I decided to participate in the Mock Draft.  I thought that this was a great place catch up on two decades of missed time, talk some baseball, learn some tricks, have some fun and maybe even make some new friends.</p>
+
+<p>I was wrong.</p>
+
+<p>I was enthusiastic.  I dove in with both feet.  I had a lot of ideas.  I had a lot of questions.  I was told to shutup, multiple times, in multiple ways, directly and indirectly, privately and publicly.</p>
+
+<p>It culminated with me drafting Myles Straw.</p>
+
+<p>When I picked Myles straw, one of the established \"pillars\" of the Scoresheet Mock Draft community rage quit the Mock Draft chat.  The feedback was immediate, and it was fairly vicious.  At that time I didn\'t have the confidence, or the desire, to engage in an all out online brawl, so I quit the draft and left my team for someone else to pick it up.</p>
+
+<p>The thing is, most Scoresheet owners use the same resources to rate their players.  They use the same metrics, take advice from the same \"experts\" and the same few established sites.</p>
+
+<p>I don\'t.  I have my own ideas, I value players using metrics that are widely available, but I don\'t use them in the same way that most owners in Scoresheet do.  I tend to value the same players that other owners do, but I occasionally find treasures that others don\'t, because I have something that is unique to me: a different perspective.  And occasionally those \"treasures\" don\'t pan out—something that can be said for anyone who plays fantasy baseball.</p>
+
+<p>But this really isn\'t about me, this isn\'t about Myles Straw, this is about online communities and the way they treat new members, and specifically new members with different perspectives.  Myles Straw just illustrates the point.</p>
+
+<p>Every online community needs noobs.  Every online community needs to treat those \"noobs\" with patience, kindness and respect.  The ones that don\'t will die.  Every healthy online community, no matter how old and well established that community is, needs constant renewal from new members and the different ideas and perspectives that they may bring.</p>',
+
+'noob_slayer.webp', 0);
+
+INSERT INTO `psss_config_help` (`id`, `enabled`, `idx`, `title`, `content`, `img`, `weight`) VALUES
+(101, 1, 1010, 'Stages of Rebuild', 
+
+'<p>In professional sports, and in fantasy sports such as Scoresheet Baseball, where you actually build and play teams, there are four pretty clearly defined stages of team building:</p>
+
+<ul>
+    <il>1. Disaster Recovery<br></il>
+    <il>2. Rebuilding<br></il>
+    <il>3. Win Now<br></il>
+    <il>4. Domination<br></il>
+</ul>
+
+<h2>Disaster Recovery</h2>
+
+<p>The first stage is the most discouraging, and the most difficult to move out of.  It tends to be a bit easier in Scoresheet in AL/NL only leagues due to high value free agents changing leagues, but in BL leagues moving out of that first stage can be extremely difficult.  It is the stage where nothing is going well, where your options for trades and reshaping your team are limited because your team will have very few assets with any value.  The end of the tunnel can seem a long distance away and very dark in this stage.</p>
+
+<p>The focus in this stage should be on accumulating assets with value, even if they don\'t match your team \"philosophy\".  Even if you don\'t like them.  In this stage you need to take advantage of high round draft picks to pick the players that have the most perceived value, regardless of position or injury status.  The key to this is research.  Find out who the most coveted players in the amateur draft are, keep an eye on the international signings, snatch up the crumbs with value that other teams let fall.  In this stage your team will not have chips to play with, and you need to accumulate those.</p>
+
+<p>Thankfully very few teams actually fall this far, but it can, and it does, happen.  If you have a team that falls into this category you need to take on the mindset of a hoarder.  Hoard those players that have value, no matter who they are.  Always be ready to take advantage of good trade offers, but in all likelihood, in this stage, your focus should be on building through the draft, and if you do make trades, ignore all positional factors, focus on players that have trade value, more than anything else.  Your team is going to lose a lot in this stage, embrace it and have fun with it.</p>
+
+<h2>Rebuilding</h2>
+
+<p>This is the stage where you need to make specific goals and plans for your team, without losing sight of the fact that winning teams win with good players.  What kind of team do you want to build?  What kind of lineup do you want to build?  Winning teams need at least something of everything, they need both run prevention and the ability to score, but you can choose to focus on one or the other, or take a balanced approach.  This is the time to start spending those chips you accumulated and focus on making smart trades.</p>
+
+<p>In some ways this is the most fun stage of  all the stages, and some owners never leave this stage, because, apparently, that\'s what they enjoy.  Winning at this stage should not be a big concern, but at some point, if your plans are well made and players are developing, you should start to see your team become more competitive.  You shouldn\'t see the unending strings of blowout losses that are a feature of the first stage.  Your team will still likely be losing a lot of games, but they should be closer, and you should see far fewer blowout losses.</p>
+
+<p>In this stage you will want to start thinking about your team\'s \"competitive window\".  Focusing on youth is a good idea, but more important than that is trying to build a team where, if all goes according to plan, most of your core players will be in their prime years at the same time.  The key to this stage is patience.  Understand that most young players entering the league for the first time will struggle.  It can take as much as five full seasons for a minor leaguer to make that transition to the major leagues, that time frame can be even longer for pitchers than it is for position players.</p>
+
+<h2>Win Now</h2>
+
+<p>This is the stage that most of the well managed teams in Scoresheet consistently exist in.  You\'ve build a solid core, you have a grand strategic vision for your team, it\'s time to fill in the holes, build depth, and spend your time playing whack-a-mole with the injury gremlins that pop up.  The keys to this stage are to not trade away too many draft picks and not protect too many minor leaguers.  If you are going to win consistently in Scoresheet you need, more than anything, to avoid Pitcher AAA, and to do that you need to draft and hoard pitching, pitching, and more pitching.</p>
+
+<p>This is the stage where you get to just \"play the game\".  Have fun with lineup strategies, plot to throw wrenches in the spokes of your fellow owner\'s bicycle wheels, relish the carnage.  As long as you are within 3 games or so of a playoff position at the end of August, and you have a full roster with some depth, you have as good a chance as anyone to win a championship.</p>
+
+<p>This is also the stage where you need to start thinking about \"maintenance\" and \"churn\".  You don\'t want to fall back into stage 1, whatever you do, so you need to think about replacing core players as they near the end of their playing years, or develop physical problems.  This is the stage where you need to start thinking about building a \"farm system\".  You don\'t want to protect too many minor leaguers, but you do want to protect a few quality ones.  The focus there should also be on value, rather than positional or philosophical objectives.  You can always trade a player with value for a piece you need.</p>
+
+<h2>Domination</h2>
+
+<p>This is the final stage of team building, and it is a stage that few teams ever reach.  It is also the most difficult to maintain, and can even be harmful to a Soresheet league\'s health.  Thankfully, in Scoresheet, it doesn\'t guarantee championships.  Scoresheet playoff mechanics ensure that any team that is within range, with some depth, at the end of August, can win a championship, you just need your team to have a good September.  So while having a dominant team can be good for the ego, it doesn\'t guarantee championships, not by a long shot.  And if you\'ve brought your team to this level, you no longer need my advice, so you aren\'t going to get any of that here.
+
+<h2>How Do I Tell What Stage My Team is In?</h2>
+
+<p>When you are mucking around in the basement of your league, and looking for some light at the end of the tunnel of lopsided losses and Mr. Pitcher AAA, it can be a little difficult to know exactly what stage you are in, and whether or not any of the advice given here applies to your team.  As is the case with this entire article, here are some ideas, you can take them or leave them, as you like.</p>
+
+<p>I use something I call \"The Star Index\", although it might be more properly called \"The Super Star Index\", as one tool to help assess what stage of team building my team is in.  While the judgment can be somewhat subjective a \"star\" for the purposes of the index rating is a player that the vast majority of baseball fans will look at and agree, \"that\'s a star\".  These are \"impact players\", players that, if you drop your team, will be a reason why a potential owner would pick up that team.</p>
+
+<p>For the purposes of this rating a player like Mookie Betts would be a \"star\", while a player like Randal Grichuk, no matter how much you may like him, would not be.  While most star players have an established track record some few players merit the \"star\" rating before they\'ve played a game in the majors.  Wander Franco was one of those.  Star players should also have some form of long term value as well, so Corbin Burnes would qualify as a \"star\" while, even though the point could be argued, perhaps Max Scherzer and Justin Verlander do not.  It can get a little messy, but most of the time it is not.</p>
+
+<p>So a quick rule of thumb would be the rule of twos.  A team in stage one, Disaster Recovery, will likely have a Star Index rating of 0.  A team in stage two, Rebuilding, would likely have a star index rating around 2  A team at the Win Now stage might have a Star Index rating of 4, while a team at the Domination stage might have a Star Index rating of 6 or better.</p>
+
+<p>Another quick way to judge how strong your team is to build a quick protected list.  Try to be objective.  A protected player should be one that is better than most of the players available in the draft, especially in a BL league, although in an AL/NL only league the players at the top of the draft, free agents that have changed leagues, will likely be better than most players that most owners protect.  I\'m also not talking about protected minor leaguers, I am talking about players that will help your team win games, and most protected minor leaguers are not going to do that, with a few notable exceptions.</p>
+
+<p>If you are having a hard time putting together a list of 13 protected players, there is a good chance you are either in stage 2 or stage 1.  If you can objectively find only 8 or less on your team that are worth protecting, there is a very good chance you are in stage 1.  Teams in stages 3 and 4 will typically have lists of players worth protecting that are much longer than 13 players.</p>
+
+<p>And of course, the final, most obvious measuring stick for your team is it\'s winning percentage.  If you have a winning percentage at the end of a season that is lower than .400, there  is a very good chance you are in either stage 1 or stage 2.  Stage 2 can run the gamut from as low as .300 to as high as .500.  I think to be legitimately in stage 3, your record in Scoresheet needs to be .500 or better, and if your record at the end of a season is .600 or better, you are flirting with stage 4, Domination.  If your team record is .700 or better, especially for more than one season running, you have probably arrived at stage 4, Domination, and some of your fellow owners might be getting discouraged.</p>',
+
+'stages_building.webp', 0);
